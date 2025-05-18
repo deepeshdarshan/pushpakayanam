e23b347ed739bb58c125f367b62bf858d82ea98a
Deepesh Raman <v-draman@expediagroup.com>
2025-05-18 20:54:09 +0530

    contact us page updated

--- js/pagevars.js
+++ js/pagevars.js
@@ -100,7 +100,7 @@ const footerHTML = `
     </div>
     <div class="container-fluid bg-dark text-secondary text-center border-top py-4 px-5"
         style="border-color: rgba(256, 256, 256, .1) !important;">
-        <p class="m-0">&copy; <a class="text-secondary border-bottom" href="#">Pushpakaayanam 2025</a>. All Rights Reserved.
+        <p class="m-0">&copy; <a class="text-secondary border-bottom" href="#">pushpakayanam.com</a>. All Rights Reserved.
     </div>
 `;
 
