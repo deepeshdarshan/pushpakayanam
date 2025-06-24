b071b231f65162357505f941d0a87f800161cac2
Deepesh Raman <v-draman@expediagroup.com>
2025-06-24 14:57:00 +0530

    bug fix

--- js/auth.js
+++ js/auth.js
@@ -1,42 +1,69 @@
 import { app } from "/js/firestore.js";
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
-const SESSION_TIMEOUT_MINUTES = 45;
+const SESSION_TIMEOUT_MINUTES = 20;
 const SESSION_KEY = 'lastPageLoadTime';
 
 const auth = getAuth(app);
 
-const loginForm = document.getElementById("loginForm");
-const errorMsg = document.getElementById("error");
-const logoutBtn = document.getElementById("logoutBtn");
-const userEmailLabel = document.getElementById("userEmail");
+export function initAuth() {
+    const loginForm = document.getElementById("loginForm");
+    const errorMsg = document.getElementById("error");
+    const logoutBtn = document.getElementById("logoutBtn");
+    const userEmailLabel = document.getElementById("userEmail");
 
-if (loginForm) {
-    loginForm.addEventListener("submit", async (e) => {
-        e.preventDefault();
+    if (loginForm) {
+        loginForm.addEventListener("submit", async (e) => {
+            e.preventDefault();
 
-        const email = loginForm.email.value;
-        const password = loginForm.password.value;
+            const email = loginForm.email.value;
+            const password = loginForm.password.value;
 
-        try {
-            const userCredential = await signInWithEmailAndPassword(auth, email, password);
-            const user = userCredential.user;
-            errorMsg.style.color = "blue";
-            errorMsg.textContent = "Logging in...";
-            console.log("Login successful:", user);
-            errorMsg.style.color = "green";
-            errorMsg.textContent = "Login successful! Please wait...";
+            try {
+                const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            } catch (error) {
+                console.error("Login error:", error);
+                errorMsg.style.color = "red";
+                errorMsg.textContent = "Invalid username or password!";
+            }
+        });
+    }
 
-            setTimeout(() => {
-                window.location.href = "/main/admin/dashboard.html";
-            }, 1500);
+    if (logoutBtn) {
+        logoutBtn.addEventListener("click", async () => {
+            logoutUser();
+        });
+    }
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
             localStorage.setItem(SESSION_KEY, Date.now());
-        } catch (error) {
-            console.error("Login error:", error);
-            errorMsg.style.color = "red";
-            errorMsg.textContent = "Invalid username or password";
+            checkSessionTimeout();
+
+            // If user is on login.html and already logged in, redirect to dashboard
+            if (window.location.pathname.includes("login.html")) {
+                gotoDashboard(user, errorMsg);
+            } else if (userEmailLabel) {
+                userEmailLabel.textContent = `${user.email}`;
+            }
+        } else {
+            localStorage.removeItem(SESSION_KEY);
+
+            // If user is not logged in and is not on login page, redirect to login
+            if (!window.location.pathname.includes("login.html")) {
+                window.location.href = "/login.html";
+            }
         }
     });
+}
+
+function gotoDashboard(user, errorMsg) {
+    console.log("Login successful:", user);
+    errorMsg.style.color = "green";
+    errorMsg.textContent = "Login successful! Please wait...";
+    setTimeout(() => {
+        window.location.href = "/main/admin/dashboard.html";
+    }, 2000);
 }
 
 async function logoutUser() {
@@ -49,33 +76,6 @@ async function logoutUser() {
         console.error("Logout failed:", error);
     }
 }
-
-if (logoutBtn) {
-    logoutBtn.addEventListener("click", async () => {
-        logoutUser();
-    });
-}
-
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        localStorage.setItem(SESSION_KEY, Date.now());
-        checkSessionTimeout();
-
-        // If user is on login.html and already logged in, redirect to dashboard
-        if (window.location.pathname.includes("login.html")) {
-            window.location.href = "/main/admin/dashboard.html";
-        } else if (userEmailLabel) {
-            userEmailLabel.textContent = `${user.email}`;
-        }
-    } else {
-        localStorage.removeItem(SESSION_KEY);
-
-        // If user is not logged in and is not on login page, redirect to login
-        if (!window.location.pathname.includes("login.html")) {
-            window.location.href = "/login.html";
-        }
-    }
-});
 
 function checkSessionTimeout() {
     const lastLoad = localStorage.getItem(SESSION_KEY);
